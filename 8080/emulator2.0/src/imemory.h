7e9e8b5326f33edfc9d706ae11ca50fb9c648935
Kumansh Furia <kumanshf@gmail.com>
2024-03-03 19:42:53 +0000

    new

--- 8080/emulator2.0/src/imemory.h
+++ 8080/emulator2.0/src/imemory.h
@@ -1,16 +1,18 @@
-#ifndef MEMORY_H
-#define MEMORY_H
+#ifndef IMEMORY_H
+#define IMEMORY_H
 
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
 
+#define MEM_SIZE 0x10000 //8080 uses 16-bit addresses (0xFFFF + 1)
+
 typedef uint8_t * iMem;
 
-iMem iMem_init(size_t size_in_bytes);
+iMem iMem_init();
 
-int load_file_into_Mem(const char * filename, uint16_t add);
+int load_file_into_Mem(const char * filename, uint16_t addr);
 
 uint8_t iMem_read_byte(uint16_t addr);
 
---
+++ 8080/emulator2.0/test/emu_test.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "i8080.h"
+#include "i8080dis.h"
+#include "memory.h"
+
